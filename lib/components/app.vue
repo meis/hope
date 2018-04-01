fa8c69063a423f2f84c8269590f3c6a8b3f91fed
Marc Egea i Sala <marc.egea.sala@gmail.com>
2018-04-01 20:53:31 +0200

    Scaffold Vuetify build

---
+++ lib/components/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title>
+        Hope
+      </v-toolbar-title>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+  };
+</script>
